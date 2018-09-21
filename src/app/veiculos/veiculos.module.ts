c94ec30bedcb2d2c0d1920b2bcaea9bd05e17307
Luiz Cunha <luiz.cunha@projetas.com.br>
2018-09-21 14:15:37 -0300

    fist commit

---
+++ src/app/veiculos/veiculos.module.ts
@@ -0,0 +1,14 @@
+import { VeiculosComponent } from './veiculos.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    VeiculosComponent
+  ]
+})
+export class VeiculosModule { }
